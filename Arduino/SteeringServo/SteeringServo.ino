bb86b5902f63997233cf9e2297f144bf0cdb94c8
Frode Lillerud <frode@enkelt.no>
2016-10-23 12:12:56 +0200

    Improved Steering testcode

--- Arduino/SteeringServo/SteeringServo.ino
+++ Arduino/SteeringServo/SteeringServo.ino
@@ -7,19 +7,27 @@
 int servoPin = 4;
 Servo servo;
 
-int pos = 0;
+int delayTime = 7; //ms. Interval [0,100]. Low turns fast, high value turns slowly.
+
+//Change values between 0 and 180 to tune servo.
+int minPos = 90;
+int maxPos = 180;
 
 void setup() {
   servo.attach(servoPin); 
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) {
+  
+  for (int pos = minPos; pos <= maxPos; pos += 1) {
     servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(delayTime);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  
+  delay(1000/delayTime);
+  for (int pos = maxPos; pos >= minPos; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo.write(pos);     // tell servo to go to position in variable 'pos'
+    delay(delayTime);     // waits 15ms for the servo to reach the position
   }
+  delay(1000/delayTime);  //Give time for servo to settle before turning other way.
 }
