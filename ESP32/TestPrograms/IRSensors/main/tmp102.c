0471b9f60f56d8dfbe74a9e33ace455d677ae5f4
Frode Lillerud <frode@lillerud.no>
2017-02-28 23:45:08 +0100

    A little further along with the TMP102 device

--- ESP32/TestPrograms/IRSensors/main/tmp102.c
+++ ESP32/TestPrograms/IRSensors/main/tmp102.c
@@ -10,9 +10,17 @@
  * Frode Lillerud, febuary 2017
  */
 
+ //https://github.com/espressif/esp-idf/blob/master/examples/peripherals/i2c/main/i2c_test.c
+ //https://github.com/nkolban/esp32-snippets/blob/master/hardware/compass/hmc5883l.c
+
  #define SDA_PIN 14
  #define SCL_PIN 13
- #define TMP102_ADDR 0x21
+ #define TMP102_ADDR 0x48
+
+ #define ACK_CHECK_ON 0x1
+ #define ACK_CHECK_OFF 0x0
+ #define ACK 0x1
+ #define NACK 0x0
 
 void task_tmp102(void *ignore)
 {
@@ -41,19 +49,42 @@ void task_tmp102(void *ignore)
     i2c_master_stop(cmd);
 
     //Send the I2C command
-    portBASE_TYPE timeout = 500 / portTICK_PERIOD_MS;
+    portBASE_TYPE timeout = 100 / portTICK_PERIOD_MS;
     error = i2c_master_cmd_begin(I2C_NUM_0, cmd, timeout);
+    i2c_cmd_link_delete(cmd);
 
-    if (error == 0) //no error
+    if (error == ESP_OK) //Device responded
     {
-      printf("Temperature: ??\n");
+      printf("TMP102 device responded.\n");
+
+      vTaskDelay(30 / portTICK_RATE_MS);
+
+      uint8_t data[2];
+
+      cmd = i2c_cmd_link_create();
+      ESP_ERROR_CHECK(i2c_master_start(cmd));
+      ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (TMP102_ADDR << 1 | I2C_MASTER_READ), ACK_CHECK_ON));
+      ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data, ACK));
+      ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data+1, NACK));
+      ESP_ERROR_CHECK(i2c_master_stop(cmd));
+
+      ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, 100 / portTICK_PERIOD_MS));
+      i2c_cmd_link_delete(cmd);
+      //if (ret == ESP_FAIL)
+      //  printf("ESP_FAIL");
+
+      printf("High: %d\n", data[0]);
+      printf("Low: %d\n", data[1]);
+
+      printf("Temperature: TODO\n");
     }
     else
     {
-      printf("ERROR: %d\n", error);
+      ESP_ERROR_CHECK(error);
+      printf("I2C ERROR: %d\n", error);
     }
 
-    i2c_cmd_link_delete(cmd); //why?
+    //i2c_cmd_link_delete(cmd); //why?
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
