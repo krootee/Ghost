9bcee5b9c706301b7c0157884ca6763005faa1ee
Frode Lillerud <frode@lillerud.no>
2017-04-08 07:26:23 +0200

    Started on a MPU6050 example.

---
+++ ESP32/MPU6050/main/main.c
@@ -0,0 +1,29 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+
+/*
+Example program for MPU-6050 accelerometer.
+mpu6050.c is from https://github.com/nkolban/esp32-snippets/tree/master/hardware/accelerometers
+*/
+
+
+#include "mpu6050.c"
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
+void app_main(void)
+{
+    xTaskCreate(&task_mpu6050, "task_mpu6050", 512, NULL, 5, NULL);
+
+    while (true) {
+        vTaskDelay(300 / portTICK_PERIOD_MS);
+    }
+}
