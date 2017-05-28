3cce131ed5180f19ca68a9a169d801a8232db2ca
Frode Lillerud <frode@lillerud.no>
2017-05-28 10:42:37 +0200

    Playing around with WebSockets

---
+++ ESP32/Ghost32Folkracer/main/WebSocketServer.cpp
@@ -0,0 +1,21 @@
+/*
+ * WebSocketServer.cpp
+ *
+ *  Created on: May 11, 2017
+ *      Author: frode
+ */
+
+#include "WebSocketServer.h"
+
+namespace Sensors {
+
+WebSocketServer::WebSocketServer(int port) {
+	// TODO Auto-generated constructor stub
+
+}
+
+WebSocketServer::~WebSocketServer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Sensors */
