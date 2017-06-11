406f90af4013a628deb9e13df85afbc8a16fa650
Frode Lillerud <frode@enkelt.no>
2017-06-11 23:14:28 +0200

    Ignore all files in build

--- MongooseOS/StartModule/build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20170611-085304/???";
-const char *build_timestamp = "2017-06-11T08:53:04Z";
-const char *build_version = "1.0";
--- MongooseOS/StartModule/build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20170611-085304/???";
-const char *mg_build_timestamp = "2017-06-11T08:53:04Z";
-const char *mg_build_version = "2017061108";
