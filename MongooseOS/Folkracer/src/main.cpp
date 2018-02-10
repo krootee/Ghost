13c09cf65bce826209990bb54d3106b7417f012e
Frode Lillerud <frode@lillerud.no>
2018-02-10 18:41:23 +0100

    Playing around with threads

--- MongooseOS/Folkracer/src/main.cpp
+++ MongooseOS/Folkracer/src/main.cpp
@@ -6,9 +6,19 @@
 #include "motor.hpp"
 #include "gp2y0e02b.hpp"
 
+#include <thread>
+
 #define GPIO_PIN_26  26
 
+void turn_on_led() {
+  mgos_gpio_set_mode(23, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_write(23, true);
+}
+
 enum mgos_app_init_result mgos_app_init(void) {
+
+  std::thread led_thread(turn_on_led);
+  led_thread.join();
 
   Actuators::Motor motor(GPIO_PIN_26);
   motor.setDesiredSpeed(50);
